db8a45af56f583203f0c5c4cccdf91a6a48faaf5
András Veres-Szentkirályi <vsza@vsza.hu>
2015-05-16 17:52:24 +0200

    find packets in long stream

--- ssdv_packet.erl
+++ ssdv_packet.erl
@@ -5,13 +5,24 @@
 
 -record(packet, {call_sign, image_id, packet_id, size, subsampling, mcu, payload}).
 
-decode_packet(Packet = <<?SYNC_BYTE, ?PACKET_TYPE_NORMAL, CallSign:32, ImageId,
+find_packets(Data) -> find_packets(Data, []).
+find_packets(Frame = <<?SYNC_BYTE, Packet:255/binary, Rest/binary>>, Acc) ->
+	case rs8:decode(Packet) of
+		{ok, ErrorCount, Data} ->
+			case decode_packet(Data) of
+				P when is_record(P, packet) -> find_packets(Rest, [P | Acc]);
+				{error, _} -> find_packets(binary:part(Frame, 1, byte_size(Frame) - 1), Acc)
+			end;
+		_ -> find_packets(binary:part(Frame, 1, byte_size(Frame) - 1), Acc)
+	end;
+find_packets(<<_, Rest/binary>>, Acc) -> find_packets(Rest, Acc);
+find_packets(_, Acc) -> Acc.
+
+decode_packet(Packet = <<?PACKET_TYPE_NORMAL, CallSign:32, ImageId,
                 PacketId:16, Width, Height, _ReservedFlags:6, SubSampling:2,
                 McuOffset, McuIndex:16, Payload:205/binary, CRC:32,
                 _FEC:32/binary>>) when Width =/= 0 andalso Height =/= 0 ->
-    RS8result = rs8:decode(binary:part(Packet, 1, 255)),
-    io:format("RS8: ~p\n", [RS8result]),
-    case erlang:crc32(binary:part(Packet, 1, 219)) of
+    case erlang:crc32(binary:part(Packet, 0, 219)) of
         CRC -> #packet{call_sign=base40:decode(CallSign), image_id=ImageId,
                        packet_id=PacketId, size={Width, Height},
                        subsampling=SubSampling, mcu=mcu(McuOffset, McuIndex),
